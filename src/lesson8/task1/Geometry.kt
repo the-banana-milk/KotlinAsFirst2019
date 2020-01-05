33b3747046eb7efad051058b2b961dbb2324a9de
mahon <mahonko_yana@mail.ru>
2020-01-05 23:25:51 +0300

    lineBySegments

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -190,7 +190,7 @@ class Line private constructor(val b: Double, val angle: Double) {
 fun lineBySegment(s: Segment): Line {
     val segmOX = abs(s.begin.x - s.end.x)
     val segmOY = abs(s.begin.y - s.end.y)
-    val ang = atan(segmOY / segmOX) % PI
+    val ang = atan(segmOY / segmOX)
     var b = 0.0
     if (s.begin.x != 0.0 || s.end.x != 0.0) b = (s.end.x * s.begin.y - s.end.y * s.begin.x) / (s.end.x - s.begin.x)
     return Line(Point(0.0, b), ang)
